431791ac6acccfad292d2c6b50290fbd22be6038
Inyanji Lynnette |  NetSec <wafulalynnette@gmail.com>
2026-09-11 08:25:48 +0300

    Add Github Actions deploy workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://inyanji-lynnette.github.io', // Your website URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Capstone-Project-TW4/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'inyanji-lynnette', // Usually your GitHub org/user name.
+  projectName: 'Capstone-Project-TW4', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
