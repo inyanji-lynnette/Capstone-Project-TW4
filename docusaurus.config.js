f4019a61ab19cef69fa2f5cfd11db350093985f8
Inyanji Lynnette |  NetSec <wafulalynnette@gmail.com>
2026-09-11 15:44:56 +0300

    Add docs content and updated sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,7 +111,7 @@ const config = {
             items: [
               {
                 label: 'Getting Started',
-                to: '/docs/intro',
+                to: '/docs/getting-started',
               },
             ],
           },
